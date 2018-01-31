eefab3856f430869f5a688017942ede060db45b8
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-01-31 17:44:47 -0600

    Stash result into features and labels

--- src/stack_data.py
+++ src/stack_data.py
@@ -4,17 +4,35 @@ import pandas as pd
 RESULTS_PATH = 'data/2017-results.csv'
 SCHEMA_PATH = 'data/2017-schema.csv'
 
-def load_data():
+CSV_COLUMN_NAMES = [
+    'Professional',
+    'ProgramHobby',
+    'Country',
+    'University',
+    'FormalEducation',
+    'MajorUndergrad',
+    'YearsProgram',
+    'YearsCodedJobPast',
+    'DeveloperType'
+]
+
+
+def load_data(label_name='DeveloperType'):
     """Parses the csv file in RESULTS_PATH."""
 
     # Parse the local CSV file.
-    train = pd.read_csv(
+    results = pd.read_csv(
         filepath_or_buffer=RESULTS_PATH,
-        # names=CSV_COLUMN_NAMES,  # list of column names
+        names=CSV_COLUMN_NAMES,  # list of column names
         header=0  # ignore the first row of the CSV file.
     )
 
-    return train
+    # 1. Assign the DataFrame's labels (the right-most column) to results_label.
+    # 2. Delete (pop) the labels from the DataFrame.
+    # 3. Assign the remainder of the DataFrame to results_features
+    results_features, results_label = results, results.pop(label_name)
+
+    return (results_features, results_label)
 
 
 print(load_data())
