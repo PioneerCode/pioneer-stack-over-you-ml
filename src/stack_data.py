0af78b63e6dac60f23c573f7dfa25d6c5a8b8d36
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-01-31 17:35:59 -0600

    Add load data function

--- src/stack_data.py
+++ src/stack_data.py
@@ -1,18 +1,20 @@
+"""Stack Overflow  DNNClassifier for the 2017 developer survey dataset."""
 import pandas as pd
 
-RAW_DATA_PATH = 'data/2016.csv'
+RESULTS_PATH = 'data/2017-results.csv'
+SCHEMA_PATH = 'data/2017-schema.csv'
 
-
-def load_data(label_name='Species'):
-    """Returns Stack Overflow Developer survey dataset."""
+def load_data():
+    """Parses the csv file in RESULTS_PATH."""
 
     # Parse the local CSV file.
     train = pd.read_csv(
-        filepath_or_buffer=RAW_DATA_PATH,
+        filepath_or_buffer=RESULTS_PATH,
         # names=CSV_COLUMN_NAMES,  # list of column names
         header=0  # ignore the first row of the CSV file.
     )
 
     return train
+
 
 print(load_data())
