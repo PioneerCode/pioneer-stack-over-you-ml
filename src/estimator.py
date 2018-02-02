979cafdb1ca7f6d921e4a9ed083567a6147aac56
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-01 20:34:36 -0600

    Update naming convention

--- src/estimator.py
+++ src/estimator.py
@@ -1,11 +1,11 @@
 """Stack Overflow  DNNClassifier for the 2017 developer survey dataset."""
-import stack_data
+import data
 
 
 def main():
     """Entry"""
     # Fetch the data
-    (train_feature, train_label), (test_feature, test_label) = stack_data.get_test_train_data()
+    (train_feature, train_label), (test_feature, test_label) = data.get_test_train_data()
     print("Training Features Shape: {0}".format(train_feature.shape))
     print("Training Label Shape: {0}".format(train_label.shape))
     print("Test Features Shape: {0}".format(test_feature.shape))
