b3dec1d8bdcf9ba445d439096678acc992463055
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-02 16:57:57 -0600

    Add data cache for inital draw

--- src/estimator.py
+++ src/estimator.py
@@ -1,14 +1,35 @@
-"""Stack Overflow  DNNClassifier for the 2017 developer survey dataset."""
-import data
+"""DNNClassifier for the Stack Overflow Developer Survey 2017 dataset."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import tensorflow as tf
+
+import stack_data
 
 
-def main():
-    """Entry"""
-    # Fetch the data
-    (train_feature, train_label), (test_feature, test_label) = data.get_test_train_data()
-    print("Training Features Shape: {0}".format(train_feature.shape))
-    print("Training Label Shape: {0}".format(train_label.shape))
-    print("Test Features Shape: {0}".format(test_feature.shape))
-    print("Test Label Shape: {0}".format(test_label.shape))
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', default=100, type=int, help='batch size')
+parser.add_argument('--train_steps', default=1000, type=int,
+                    help='number of training steps')
 
-main()
+
+def main(argv):
+    args = parser.parse_args(argv[1:])
+
+    # Fetch the stack_data
+    (train_x, train_y), (test_x, test_y) = stack_data.get_test_train_data()
+    print("Training Features Shape: {0}".format(train_x.shape))
+    print("Training Label Shape: {0}".format(train_y.shape))
+    print("Test Features Shape: {0}".format(test_x.shape))
+    print("Test Label Shape: {0}".format(test_y.shape))
+
+    # Feature columns describe how to use the input.
+    my_feature_columns = []
+    for key in train_x.keys():
+
+
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run(main)
--- src/stack_data.py
+++ src/stack_data.py
@@ -8,11 +8,14 @@ import os
 import zipfile
 import data_clean
 import pandas as pd
+import tensorflow as tf
 
 ARCHIVE_PATH = 'data/developer_survey_2017.zip'
 RESULTS_PATH = 'data/unpacked/survey_results_public.csv'
 SCHEMA_PATH = 'data/unpacked/survey_results_schema.csv'
-CLEAN_PATH = 'data/unpacked/cleaned_survey_results_public.csv'
+CLEAN_PATH = 'data/unpacked/cleaned/cleaned_survey_results_public.csv'
+LABELS = ['Sentosa', 'Versicolor', 'Virginica']
+
 
 def get_test_train_data():
     """Parses the csv file in RESULTS_PATH."""
@@ -20,34 +23,32 @@ def get_test_train_data():
     # TODO: If we already have a cleaned dataset, lets just return that
     # Insure files are on disk
     maybe_unzip()
+    raw_data = None
 
     # Parse the local CSV file.
-    raw_data = get_dataframe_from_csv()
+    if not os.path.isfile(CLEAN_PATH):
+        raw_data = pd.read_csv(
+            filepath_or_buffer=RESULTS_PATH,
+            header=0,
+            low_memory=False
+        )
+        output = data_clean.FEATURE_COLUMNS
+        output.append(data_clean.LABEL_NAME)
+        raw_data.loc[:, output].to_csv(CLEAN_PATH, index=False)
+    else:
+        raw_data = pd.read_csv(
+            filepath_or_buffer=CLEAN_PATH,
+            header=0,
+            low_memory=False
+        )
 
-    # results.loc[:, FEATURE_COLUMNS].to_csv(CLEAN_PATH, index=False)
     return data_clean.get_clean_data(raw_data)
 
 
 def maybe_unzip():
     """Unpack Archive if not already unpacked"""
-    if do_archived_files_exist is not True:
+    if os.path.isfile(RESULTS_PATH) is not True:
         zip_ref = zipfile.ZipFile(ARCHIVE_PATH, 'r')
         zip_ref.extractall('data/unpacked')
         zip_ref.close()
 
-
-def get_dataframe_from_csv():
-    """Load data into dataframe"""
-    return pd.read_csv(
-        filepath_or_buffer=RESULTS_PATH,
-        header=0,  # ignore the first row of the CSV file.
-        low_memory=False
-    )
-
-
-def do_archived_files_exist():
-    """Check if files are on disk"""
-    if os.path.isfile(RESULTS_PATH):
-        return True
-
-    return False
