0632cf766ce195ae63c28a61c4aee3eff1697474
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-19 06:33:05 -0600

    Re-org data load

--- src/data/get_2017_data.py
+++ src/data/get_2017_data.py
@@ -2,9 +2,14 @@ import os
 import pandas as pd
 import zipfile
 
-ARCHIVE_PATH = 'data/developer_survey_2017.zip'
-RESULTS_PATH = 'data/unpacked/survey_results_public.csv'
-CACHE_PATH = 'data/cache'
+PARENT = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+ARCHIVE_FILE_PATH = os.path.join(
+    PARENT, 'data', 'raw', 'developer_survey_2017.zip')
+UNPACKED_PATH = os.path.join(PARENT, 'data', 'raw', 'unpacked')
+RAW_FILE_PATH = os.path.join(UNPACKED_PATH, 'survey_results_public.csv')
+CACHE_PATH = os.path.join(PARENT, 'data', 'raw', 'cache')
+CACHE_FILE_PATH = os.path.join(CACHE_PATH, 'cached_survey_results_public.csv')
+
 FEATURE_COLUMNS = [
     'Professional',
     'ProgramHobby',
@@ -17,34 +22,51 @@ FEATURE_COLUMNS = [
 LABEL_NAME = 'DeveloperType'
 
 
-def maybe_unpack():
-    # Unpack Archive if not already unpacked
-    if os.path.isfile(RESULTS_PATH) is not True:
-        zip_ref = zipfile.ZipFile(ARCHIVE_PATH, 'r')
-        zip_ref.extractall('data/unpacked')
-        zip_ref.close()
-
-def load_data():
-    # Builds column collection
+def get_features_and_columns():
+    '''
+    Get list of features and label
+    '''
     names = FEATURE_COLUMNS
     names.append(LABEL_NAME)
+    return names
 
-    # load data
-    if not os.path.isfile('data/cache/cached_survey_results_public.csv'):
-        raw_data = pd.read_csv(
-            'data/unpacked/survey_results_public.csv',
-            header=0,
-            low_memory=False
-        )
-        raw_data = raw_data.loc[:, names]
-        raw_data = raw_data.to_csv('data/cache/cached_survey_results_public.csv', index=False)
+
+def maybe_unpack():
+    '''
+    Unpack Archive if not already unpacked
+    '''
+    if os.path.isfile(UNPACKED_PATH) is not True:
+        zip_ref = zipfile.ZipFile(ARCHIVE_FILE_PATH, 'r')
+        zip_ref.extractall(UNPACKED_PATH)
+        zip_ref.close()
+
+
+def load_data():
+    '''
+    Get raw data based on features and label list
+    '''
+    if not os.path.isfile(CACHE_FILE_PATH):
+        print(RAW_FILE_PATH)
+
+        raw_data = pd.read_csv(RAW_FILE_PATH,
+                               header=0,
+                               low_memory=False
+                               )
+        raw_data = raw_data.loc[:, get_features_and_columns()]
+        raw_data = raw_data.to_csv(
+            CACHE_FILE_PATH, index=False)
     else:
-        raw_data = pd.read_csv('data/cache/cached_survey_results_public.csv', 
-                            low_memory=False)
+        raw_data = pd.read_csv(CACHE_FILE_PATH,
+                               low_memory=False)
 
     return raw_data
 
+
 def get_data():
+    '''
+    Get raw dataframe of 2017 data
+    '''
+    # Create cache directory
     if not os.path.exists(CACHE_PATH):
         os.makedirs(CACHE_PATH)
 
@@ -53,4 +75,3 @@ def get_data():
 
 
 get_data()
-
